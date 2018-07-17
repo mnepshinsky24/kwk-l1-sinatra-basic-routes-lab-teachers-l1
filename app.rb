24d7c94dcd29f14222ba407d97d3be875411b69d
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-17 00:12:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,22 +4,15 @@ class App < Sinatra::Base
   get '/' do 
     "Hello, World!"
   end 
-end
 
-
-class App < Sinatra::Base 
   get '/name' do 
         "My name is _" 
   end 
-end 
-
-class App < Sinatra::Base 
+ 
   get '/hometown' do 
         "My hometown is _" 
   end 
-end 
 
-class App < Sinatra::Base 
   get '/favorite-song' do 
         "My favorite song is _" 
   end 
